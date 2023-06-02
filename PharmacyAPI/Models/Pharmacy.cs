da9ce444c3f0b40f6f9e0723f8c5f5461a4af452
bfrankie <bfrank0193@gmail.com>
2023-06-02 15:09:33 -0500

    Create Pharmacy model

---
+++ PharmacyAPI/Models/Pharmacy.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PharmacyAPI.Models
+{
+	public class Pharmacy
+	{
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Address { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string City { get; set; }
+
+        [Required]
+        [StringLength(2, ErrorMessage = "State should be in two-letter format.")]
+        public string State { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d{5}([-]|\s*)?(\d{4})?$", ErrorMessage = "Please enter a valid 5-9 digit zipcode")]
+        public string Zip { get; set; }
+
+        public int? FilledPrescriptions { get; set; } = 0;
+
+        [Required]
+        [Editable(false)]
+        public DateTime CreatedAt { get; } = DateTime.Now;
+
+        [Editable(false)]
+        public DateTime? UpdatedAt { get; }
+    }
+}
