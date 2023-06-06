fd2c41129b002121037a98d9a21f7c77dac99b3c
bfrankie <bfrank0193@gmail.com>
2023-06-06 12:46:10 -0500

    Complete unit testing for service layer

---
+++ PharmacyAPI.Tests/Services/PharamcyServiceTests.cs
@@ -0,0 +1,101 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using Moq;
+using PharmacyAPI.Models;
+using PharmacyAPI.Repositories;
+using PharmacyAPI.Services;
+
+namespace PharmacyAPI.Tests.Services
+{
+    public class PharamcyServiceTests
+	{
+
+        private readonly Mock<IPharmacyRepository> _repositoryMock;
+        private readonly PharmacyService _serviceMock;
+        private readonly IFixture _fixture;
+
+        public PharamcyServiceTests()
+        {
+            _fixture = new Fixture();
+            _repositoryMock = _fixture.Freeze<Mock<IPharmacyRepository>>();
+            _serviceMock = new PharmacyService(_repositoryMock.Object);
+        }
+
+        [Fact]
+        public async Task GetAllPharmacies_ShouldReturnListOfAllPharmacies_WhenPharmaciesFound()
+        {
+            var pharmacyListMock = _fixture.Create<List<Pharmacy>>();
+            _repositoryMock.Setup(x => x.GetAllPharmacies()).ReturnsAsync(pharmacyListMock);
+
+            var result = await _serviceMock.GetAllPharmacies();
+
+            result.Should().NotBeNull();
+            result.Should().BeAssignableTo<List<Pharmacy>>();
+        }
+
+        [Fact]
+        public async Task GetAllPharmacies_ShouldReturnEmptyList_WhenNoPharmaciesFound()
+        {
+            _fixture.OmitAutoProperties = true;
+            var pharmacyListMock = new List<Pharmacy>();
+            _repositoryMock.Setup(x => x.GetAllPharmacies()).ReturnsAsync(pharmacyListMock);
+            var result = await _serviceMock.GetAllPharmacies();
+      
+            result.Should().BeAssignableTo<List<Pharmacy>>();
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task GetPharmacyById_ShouldReturnSinglePharmacy_WhenIdIsFound()
+        {
+            var pharmacyMock = _fixture.Create<Pharmacy>();
+            var id = _fixture.Create<int>();
+            _repositoryMock.Setup(x => x.GetPharmacyById(id)).ReturnsAsync(pharmacyMock);
+
+            var result = await _serviceMock.GetPharmacyById(id);
+
+            result.Should().NotBeNull();
+            result.Should().BeAssignableTo<Pharmacy>();
+        }
+
+        [Fact]
+        public async Task GetPharmacyById_ShouldReturnNull_WhenIdIsNotFound()
+        {
+            Pharmacy? pharmacyMock = null;
+            var id = _fixture.Create<int>();
+            _repositoryMock.Setup(x => x.GetPharmacyById(id)).ReturnsAsync(pharmacyMock);
+
+            var result = await _serviceMock.GetPharmacyById(id);
+
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task UpdatePharmacyById_ShouldReturnUpdatedPharmacy_WhenInputIsValidAndExistingPharmacyIsFound()
+        {
+            var pharmacyMock = _fixture.Create<Pharmacy>();
+            var id = _fixture.Create<int>();
+            _repositoryMock.Setup(x => x.UpdatePharmacyById(id, pharmacyMock)).ReturnsAsync(pharmacyMock);
+
+            var result = await _serviceMock.UpdatePharmacyById(id, pharmacyMock);
+
+            result.Should().NotBeNull();
+            result.Should().BeAssignableTo<Pharmacy>();
+            result.Should().BeEquivalentTo(pharmacyMock);
+        }
+
+        [Fact]
+        public async Task UpdatePharmacyById_ShouldReturnNull_WhenNoExistingPharmacyIsFound()
+        {
+            Pharmacy? nullPharmacyMock = null;
+            var pharmacyMock = _fixture.Create<Pharmacy>();
+            var id = _fixture.Create<int>();
+            _repositoryMock.Setup(x => x.UpdatePharmacyById(id, pharmacyMock)).ReturnsAsync(nullPharmacyMock);
+
+            var result = await _serviceMock.UpdatePharmacyById(id, pharmacyMock);
+
+            result.Should().BeNull();
+        }
+    }
+}
+
---
+++ PharmacyAPI.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace PharmacyAPI.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
