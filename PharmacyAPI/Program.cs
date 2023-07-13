e5f352bee93036cce9af212c0f52d03a32516674
Rinn <75754885+Rinn5577@users.noreply.github.com>
2023-07-13 15:12:43 -0500

    CORs fix
    Fixed cors issue, can now access API successfully.

--- PharmacyAPI/Program.cs
+++ PharmacyAPI/Program.cs
@@ -11,27 +11,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<PharmacyDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("PharmacyDB")));
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: MyAllowSpecificOrigins,
-        policay =>
-        {
-            options.AddPolicy(name: MyAllowSpecificOrigins,
-                  policy =>
-                  {
-                      policy.WithOrigins("http://localhost:3000")
-                      .AllowAnyHeader()
-                      .AllowAnyMethod();
-                  });
-        });
-});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IPharmacyService, PharmacyService>();
 builder.Services.AddScoped<IPharmacyRepository, PharmacyRepository>();
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:3000")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                      });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -39,7 +34,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-} 
+}
 
 app.UseHttpsRedirection();
 
@@ -50,4 +45,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
--- pharmacy-ui/src/service/pharmacyService.ts
+++ pharmacy-ui/src/service/pharmacyService.ts
@@ -4,6 +4,7 @@ import { PharmacyModel } from '../models/redux-models'
 export default{
     async getAllPharmacies(){
         var response=await api().get("");
+        console.log(response.data)
         return response.data;
     },
     async getParticularPharmacy(pharmacy_id:number){
