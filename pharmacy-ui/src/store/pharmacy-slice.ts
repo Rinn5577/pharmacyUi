c9af4cc554ba7e71da70ca2b862a7f07e118dbf2
Rinn <75754885+Rinn5577@users.noreply.github.com>
2023-07-13 11:03:47 -0500

    Update pharmacy-slice.ts
    defined initial state for pharmacies and created slice for store

---
+++ pharmacy-ui/src/store/pharmacy-slice.ts
@@ -0,0 +1,31 @@
+import { PharmacyModel, PharmacyArrayModel } from "../models/redux-models";
+import {createSlice,PayloadAction} from "@reduxjs/toolkit";
+
+const initialPharmacyState:PharmacyArrayModel={
+    all_pharmacies:[],
+    particular_pharmacy:{
+        "id": 0,
+        "name": "",
+        "address": "",
+        "city": "",
+        "state": "",
+        "zipcode": "",
+        "filledPrescriptionsMonthToDate": 0,
+        "createdAt": "",
+        "updatedAt": ""
+    }
+}
+
+const pharmacySlice=createSlice({
+    name:'pharmacy',
+    initialState: initialPharmacyState,
+    reducers:{
+        setPharmacies(state,action:PayloadAction<PharmacyModel[]>){
+            state.all_pharmacies=action.payload;
+        },
+        setParticularTodo(state,action:PayloadAction<PharmacyModel>){
+            state.particular_pharmacy=action.payload;
+        }
+    }
+})
+export default pharmacySlice;
