a263e93672c5183c44c39cebc22642afd2512b4a
Rinn <75754885+Rinn5577@users.noreply.github.com>
2023-07-15 17:48:52 -0500

    check that pharmacy state exists
    currently checks if there is a pharmacy with an id other than 0 set in state, if theres not the update form does not render.

--- pharmacy-ui/src/components/Playground.tsx
+++ pharmacy-ui/src/components/Playground.tsx
@@ -6,50 +6,32 @@ import Search from "./Search";
 import PharmacyView from "./PharmacyView";
 
 const Pharmacy=()=>{
-    // const dispatch=useAppDispatch();
-    // const pharmacyList=useAppSelector(state=>state.pharmacy.pharmacy_list);
+    //const dispatch=useAppDispatch();
+    const pharmacy=useAppSelector(state=>state.pharmacy.pharmacy);
+    console.log(pharmacy)
     // const clickHandler=()=>[
     //     dispatch(fetchPharmacyList())
     // ]
 
-    // const checkPharmacyList=():boolean=>{
-    //     if(pharmacyList.length==0){
-    //         return false
-    //     }
-    //     return true
-    // }
+    const checkPharmacy=():boolean=>{
+        if(pharmacy.id === 0){
+            return false
+        }
+        return true
+    }
 
 
     return(
         <>
         <PharmacyView></PharmacyView>
-        {/* <Search></Search> */}
-
-        {/* <div>
-            <Button variant="outlined" onClick={clickHandler} >All Pharmacies</Button>
+        {
+            checkPharmacy() &&
             <div>
-                <h3>Pharmacy List: </h3>
-                <div className="pharmacy-container">
-                    <p className="pharmacy-child1">ID</p>
-                    <p className="pharmacy-child2">NAME</p>
-                    <p className="pharmacy-child3">CREATED</p>
-                </div>
-                {checkPharmacyList() &&
-                    pharmacyList.map((pharmacy)=>(
-                        <div className="pharmacy-container" key={pharmacy.id}>
-                        <p className="pharmacy-child1">{pharmacy.id}</p>
-                        <p className="pharmacy-child2">{pharmacy.name}</p>
-                        <p className="pharmacy-child3">{pharmacy.filledPrescriptionsMonthToDate}</p>
-                        <p className="pharmacy-child4">{pharmacy.createdAt}</p>
-
-                        </div>
-                    ))
-                }
+                <UpdateForm></UpdateForm>
             </div>
-        </div> */}
-        {/* <div>
-            <UpdateForm></UpdateForm>
-        </div> */}
+
+        }
+
         </>
 
     )
