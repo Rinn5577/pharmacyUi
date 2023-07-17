8666e8667583bbb78c875e23e5ce81731c1d478c
Rinn <75754885+Rinn5577@users.noreply.github.com>
2023-07-17 02:36:29 -0500

    tailwind and updateForm
    added tailwind and reconfigured the update form to use it

--- pharmacy-ui/src/App.tsx
+++ pharmacy-ui/src/App.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Routes, Route, Link, Router, BrowserRouter } from "react-router-dom";
-import './App.css';
-import Playground from './components/Playground';
 import NavBar from './components/NavBar';
-import UpdatePharmacy from './pages/UpdatePharmacy';
-import Home from './pages/Home';
 
 function App() {
   return (
--- pharmacy-ui/src/components/UpdateForm.tsx
+++ pharmacy-ui/src/components/UpdateForm.tsx
@@ -1,4 +1,3 @@
-import { Button, Stack, TextField } from "@mui/material";
 import React, {useState} from "react"
 import { useAppDispatch, useAppSelector } from "../hooks/redux-hooks";
 import { postPharmacyUpdate } from "../store/pharmacy-actions";
@@ -15,9 +14,10 @@ const [updatedPharmacy, setUpdatedPharmacy] = useState(pharmacy)
 const dispatch=useAppDispatch();
 
 
+
 // this takes in the event of the userinput 
 //it sets the updatedPharmacy we created with the info it already has and adds in the extra info 
-const changeHandler = (event : React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+const changeHandler = (event : React.ChangeEvent<HTMLInputElement>) => {
     setUpdatedPharmacy({...updatedPharmacy,[event.target.name] : event.target.value})
 }
 
@@ -35,58 +35,49 @@ function handleSubmit(event:any){
         
         <>
 
-<h2>Update Pharmacy Form</h2>
-<form onSubmit={handleSubmit}>
-<Stack>
-    <TextField
-    type="text"
-    variant='outlined'
-    color='secondary'
-    label="Pharmacy Name"
-    onChange={(e) => changeHandler(e)}
-    value={updatedPharmacy.name}
-    name="name"
-    />
-    <TextField
-    type="text"
-    variant='outlined'
-    color='secondary'
-    label="Street Address"
-    onChange={(e) => changeHandler(e)}
-    value={updatedPharmacy.address}
-    name="address"
-    />
-    <TextField
-    type="text"
-    variant='outlined'
-    color='secondary'
-    label="City"
-    onChange={(e) => changeHandler(e)}
-    value={updatedPharmacy.city}
-    name="city"
-    />
-    <TextField
-    type="text"
-    variant='outlined'
-    color='secondary'
-    label="State"
-    onChange={(e) => changeHandler(e)}
-    value={updatedPharmacy.state}
-    name="state"
-    />
-    
-    <TextField
-    type="text"
-    variant='outlined'
-    color='secondary'
-    label="Zipcode"
-    onChange={(e) => changeHandler(e)}
-    value={updatedPharmacy.zipcode}
-    name="zipcode"
-    />
-</Stack>
-<Button variant="outlined" type="submit">Update</Button>
-</form>
+            <form className="w-full max-w-lg">
+            <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                    Pharmacy Name
+                </label>
+                <input  onChange={(e) => changeHandler(e)} name="name" value={updatedPharmacy.name} className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"  type="text" placeholder={pharmacy.name}/>
+                </div>
+            </div>
+            <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full px-3">
+                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                    Street Address
+                </label>
+                <input onChange={(e) => changeHandler(e)} name="address" value={updatedPharmacy.address} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" placeholder={pharmacy.address}/>
+                </div>
+            </div>
+            <div className="flex flex-wrap -mx-3 mb-2">
+                <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                    City
+                </label>
+                <input onChange={(e) => changeHandler(e)} name="city" value={updatedPharmacy.city} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"  type="text" placeholder={pharmacy.city}/>
+                </div>
+                <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                    State
+                </label>
+                <input onChange={(e) => changeHandler(e)}  name="state" value={updatedPharmacy.state} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" placeholder={pharmacy.state}/>
+                </div>
+                <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                    Zip
+                </label>
+                <input onChange={(e) => changeHandler(e)}  name ="zipcode" value={updatedPharmacy.zipcode} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" placeholder={pharmacy.zipcode}/>
+                </div>
+            </div>
+            <div className="flex items-center justify-between">
+                <button onClick={(e) => handleSubmit(e)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                    Update
+                </button>
+                </div>
+            </form>
 
         </>
     )
