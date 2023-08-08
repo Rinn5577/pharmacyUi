20920c5a439b791f80a8b56cde29482a87bad810
Rinn <75754885+Rinn5577@users.noreply.github.com>
2023-08-08 11:28:05 -0500

    fixed key values

--- pharmacy-ui/src/components/Form/Selector.tsx
+++ pharmacy-ui/src/components/Form/Selector.tsx
@@ -28,9 +28,10 @@ const Selector: React.FC<Props> = ({
         className="appearance-none w-max bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
       >
         {optionValue.map((option) => (
-          <option key={option} value={option}>
+          <option key={option || "default"} value={option}>
             {option === "default" ? "Select" : option}
           </option>
+
         ))}
       </select>
     </div>
